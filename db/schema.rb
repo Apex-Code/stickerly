d31af13eeaf4eff584cad6f4e77a3cc212297e02
Apex-Code <upstaterounder@gmail.com>
2017-07-28 23:34:57 -0500

    migrate db, and require stylesheets

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729023503) do
+ActiveRecord::Schema.define(version: 20170729035448) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
