4935bde7e471898ed3f89731793dc2d69c1bbe32
Apex-Code <upstaterounder@gmail.com>
2017-07-28 23:50:20 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   resources :notes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+	  root "notes#index", as: "authenticated_root"
+	end
+
+	root "welcome#index"
 end
