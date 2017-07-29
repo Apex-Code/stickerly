2d78ae3f295718692c8117978dee876add1db8c4
Apex-Code <upstaterounder@gmail.com>
2017-07-28 19:57:38 -0500

    add new create edit update show and destroy as well as private methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,30 @@
 class NotesController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+private
+
+  def find_note
+  end
+  
+  def note_params
+  	params.require(:note).permit(:title, :content)
+
 end
