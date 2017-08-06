2107c10ac1a3cb0aeb31c952ddbb994d710de286
Dave Mars <upstaterounder@gmail.com>
2017-08-06 15:44:03 -0500

    Update 20170729035448_add_user_id_to_notes.rb
    fix indents

--- db/migrate/20170729035448_add_user_id_to_notes.rb
+++ db/migrate/20170729035448_add_user_id_to_notes.rb
@@ -1,8 +1,6 @@
 class AddUserIdToNotes < ActiveRecord::Migration[5.1]
   def change
-
-  	add_column :notes, :user_id, :integer
+    add_column :notes, :user_id, :integer
     add_index :notes, :user_id
-    
   end
 end
